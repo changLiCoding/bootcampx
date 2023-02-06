27bd9fefcb93813d123f4031f887857e81f7419d
javascriptsucks <evix1988@live.cn>
2023-02-06 16:06:14 -0600

    upload-first-query-file

---
+++ 0_selects/1_students_without_github.sql
@@ -0,0 +1,13 @@
+SELECT id, name, email, cohort_id
+FROM students
+WHERE github IS NULL
+ORDER BY cohort_id;
+
+
+
+
+
+-- SELECT id, name, email, cohort_id
+-- FROM students
+-- WHERE github IS NULL
+-- ORDER BY cohort_id
